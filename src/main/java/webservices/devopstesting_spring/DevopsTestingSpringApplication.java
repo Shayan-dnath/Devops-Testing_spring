19c55c98375291b526ed51bd89c2215c2bb2a5b1
Shayan Debnath <shayandevnath@gmail.com>
2024-04-04 15:18:47 +0530

    Initial commit

---
+++ src/main/java/webservices/devopstesting_spring/DevopsTestingSpringApplication.java
@@ -0,0 +1,13 @@
+package webservices.devopstesting_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTestingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsTestingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/webservices/devopstesting_spring/DevopsTestingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package webservices.devopstesting_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTestingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
